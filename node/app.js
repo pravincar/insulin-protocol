e37852c656c95b5e136a60ecff8f79c6f0624e5a
Pravin Carvalho <pcarvalho@sunriselabs.com>
2022-05-15 23:20:50 +0530

    Auto format document

--- node/app.js
+++ node/app.js
@@ -15,26 +15,26 @@ app.use('/data', require('./routes/data').router);
 // Serve files in the dist directory as static files
 app.use(express.static('dist'));
 
-app.get('*',function(req, res){
-    res.sendFile('index.html',{root:'dist'});
+app.get('*', function (req, res) {
+  res.sendFile('index.html', { root: 'dist' });
 });
 
 // catch 404 and forward to error handler
-app.get(/(\.)+/,function(req, res, next) {
+app.get(/(\.)+/, function (req, res, next) {
   var err = new Error('404: Please check server logs');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-  console.log('express_error',err);
+  console.log('express_error', err);
   // render the error page
   res.status(err.status || 500);
-  res.send(err.status+': Not Found');
+  res.send(err.status + ': Not Found');
 });
 
 module.exports = app;
